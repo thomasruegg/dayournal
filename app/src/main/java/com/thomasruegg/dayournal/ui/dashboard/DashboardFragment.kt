75f3c7f6ea666c9134d8a77ba54f0481e8969189
thomasruegg <hello@thomasruegg.com>
2023-10-11 18:52:28 +0200

    implemented jump to today functionality

--- app/src/main/java/com/thomasruegg/dayournal/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/thomasruegg/dayournal/ui/dashboard/DashboardFragment.kt
@@ -4,12 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.CalendarView
 import androidx.fragment.app.Fragment
 //import androidx.lifecycle.ViewModelProvider
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.thomasruegg.dayournal.R
 import com.thomasruegg.dayournal.databinding.FragmentDashboardBinding
+import java.util.Calendar
 
 class DashboardFragment : Fragment() {
 
@@ -35,6 +36,16 @@ class DashboardFragment : Fragment() {
 //            textView.text = it
 //        }
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val calendar : CalendarView = view.findViewById(R.id.calendar)
+        val currentTimeInMillis = Calendar.getInstance().timeInMillis
+        val todayButton : FloatingActionButton = view.findViewById(R.id.todayButton)
+        todayButton.setOnClickListener {
+            calendar.setDate(currentTimeInMillis, true, true)
+        }
     }
 
     override fun onDestroyView() {
