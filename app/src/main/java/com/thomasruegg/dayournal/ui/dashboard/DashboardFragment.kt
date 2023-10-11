acfc08d674a896a9882b3f50cb5ca72e0425e818
thomasruegg <hello@thomasruegg.com>
2023-10-11 18:51:55 +0200

    uncommented viewModel

--- app/src/main/java/com/thomasruegg/dayournal/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/thomasruegg/dayournal/ui/dashboard/DashboardFragment.kt
@@ -6,7 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+//import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.thomasruegg.dayournal.R
 import com.thomasruegg.dayournal.databinding.FragmentDashboardBinding
 
 class DashboardFragment : Fragment() {
@@ -22,16 +24,16 @@ class DashboardFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
+//        val dashboardViewModel =
+//            ViewModelProvider(this).get(DashboardViewModel::class.java)
 
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+//        val textView: TextView = binding.textDashboard
+//        dashboardViewModel.text.observe(viewLifecycleOwner) {
+//            textView.text = it
+//        }
         return root
     }
 
